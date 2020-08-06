d332abb719be7f5f58379d12d0ed63b99199e4e6
Harrison Ifeanyichukwu <Harrisonifeanyichukwu@gmail.com>
2020-08-06 21:04:16 +0200

    fix: added typings

---
+++ src/typings/elements.d.ts
@@ -0,0 +1,25 @@
+import { BlockNodeType, EmptyBlockNodeType, SpanNodeType, RichTextSpanProps } from '.';
+
+export type ElementType = BlockNodeType | EmptyBlockNodeType | SpanNodeType;
+
+export interface BaseElementProps {
+  type: ElementType;
+}
+
+export interface BlockElementProps extends BaseElementProps {
+  type: BlockNodeType;
+  text: string;
+  spans: RichTextSpanProps[];
+}
+
+export interface EmptyBlockElementProps extends BaseElementProps {
+  type: EmptyBlockNodeType;
+}
+
+export interface SpanElementProps extends BaseElementProps {
+  type: SpanNodeType;
+  text: string;
+  data?: any;
+}
+
+export type Element = BlockElementProps | EmptyBlockElementProps | SpanElementProps;
---
+++ src/typings/index.d.ts
@@ -0,0 +1,78 @@
+export type SpanNodeType = 'hyperlink' | 'em' | 'strong' | 'label' | 'text';
+
+export type BlockNodeType =
+  | 'heading1'
+  | 'heading2'
+  | 'heading3'
+  | 'heading4'
+  | 'heading5'
+  | 'heading6'
+  | 'paragraph'
+  | 'preformated'
+  | 'list-item'
+  | 'o-list-item'
+  | 'group-list-item'
+  | 'group-o-list-item';
+
+export type EmptyBlockNodeType = 'image' | 'embed';
+
+export type RichTextNodeType = BlockNodeType | EmptyBlockNodeType;
+
+export interface RichTextLinkToProps {
+  url: string;
+  target: string;
+}
+
+export interface RichTextOEmbedProps {
+  embed_url: string;
+  provider_name: string;
+  type: string;
+  label?: string;
+}
+
+export interface RichTextImageProps {
+  linkTo?: RichTextLinkToProps;
+  label?: string;
+  url: string;
+  alt?: string;
+}
+
+export interface RichTextSpanProps {
+  start: number;
+  end: number;
+  type: SpanNodeType;
+  data: any;
+}
+
+export interface RichTextSpanWithTextProps extends RichTextSpanProps {
+  text: string;
+}
+
+export interface RichTextBaseNodeProps {
+  type: RichTextNodeType;
+}
+
+export interface RichTextBlockNodeProps extends RichTextBaseNodeProps {
+  type: BlockNodeType;
+  text: string;
+  spans: RichTextSpanProps[];
+}
+
+export interface RichTextImageNodeProps
+  extends RichTextBaseNodeProps,
+    RichTextImageProps {
+  type: 'image';
+}
+
+export interface RichTextOEmbedNodeProps
+  extends RichTextBaseNodeProps,
+    RichTextOEmbedProps {
+  type: 'embed';
+}
+
+export type RichTextNodeProps =
+  | RichTextBlockNodeProps
+  | RichTextImageNodeProps
+  | RichTextOEmbedNodeProps;
+
+export type RichTextProps = Array<RichTextNodeProps>;
